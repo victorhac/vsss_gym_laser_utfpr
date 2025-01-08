f0165b8b8a0aa7cbaa913ff8622c3bcb7b2c70fc
Victor Amorim <victorhac231@gmail.com>
2025-01-08 12:06:50 -0300

    8: game state machine added

--- lib/state_machine/game_state_machine.py
+++ lib/state_machine/game_state_machine.py
@@ -5,141 +5,124 @@ from lib.domain.referee_message import RefereeMessage
 from lib.utils.game_state_machine_utils import GameStateMachineUtils
 
 class GameStateMachineModel():
-    def __init__(self, is_yellow_team: bool):
-        self.is_yellow_team = is_yellow_team
+    def set_state_by_referee_message(
+        self,
+        message: RefereeMessage
+    ):        
+        get_trigger_name = GameStateMachineUtils.get_trigger_method_name(
+            message.foul_enum,
+            message.is_yellow_team
+        )
 
-    def set_state_by_referee_message(self, message: RefereeMessage):
-        team_foe_team_foul_enums = [
-            FoulEnum.FREE_KICK,
-            FoulEnum.GOAL_KICK,
-            FoulEnum.KICKOFF,
-            FoulEnum.FREE_BALL]
-        
-        def get_trigger_name(foul_enum, is_yellow_team = None):
-            return GameStateMachineUtils.get_trigger_name(
-                foul_enum,
-                is_yellow_team
-            )
+        getattr(
+            self,
+            get_trigger_name,
+            None
+        )()
 
-        if message.foul_enum in team_foe_team_foul_enums:
-            getattr(
-                self,
-                get_trigger_name(
-                    message.foul_enum,
-                    message.is_yellow_team
-                ),
-                None
-            )()
-        else:
-            getattr(
-                self,
-                get_trigger_name(message.foul_enum),
-                None
-            )()
-
-class GameStateMachine(Machine):
+class GameStateMachine:
     def __init__(
         self,
         is_yellow_team: bool
     ):
+        self.model = GameStateMachineModel()
         self.is_yellow_team = is_yellow_team
         self.initial_state = FoulEnum.HALT
 
-        def foul_enum_name(foul_enum, is_team = None):
-            return GameStateMachineUtils.foul_enum_name(
+        get_foul_enum_name = lambda foul_enum, is_team = None:\
+            GameStateMachineUtils.get_foul_enum_name(
                 foul_enum,
                 is_team
             )
         
-        def get_state_name(is_yellow, foul_enum):
-            return GameStateMachineUtils.get_state_name(
-                is_yellow,
+        get_state_name = lambda foul_enum, is_yellow:\
+            GameStateMachineUtils.get_state_name(
                 foul_enum,
+                is_yellow,
                 self.is_yellow_team
             )
         
-        def get_trigger_name(foul_enum, is_trigger_yellow = None):
-            return GameStateMachineUtils.get_trigger_name(
+        get_trigger_name = lambda foul_enum, is_trigger_yellow = None:\
+            GameStateMachineUtils.get_trigger_name(
                 foul_enum,
                 is_trigger_yellow
             )
 
-        model = GameStateMachineModel()
         states = [
-            foul_enum_name(FoulEnum.FREE_KICK, True),
-            foul_enum_name(FoulEnum.FREE_KICK, False),
-            foul_enum_name(FoulEnum.GOAL_KICK, True),
-            foul_enum_name(FoulEnum.GOAL_KICK, False),
-            foul_enum_name(FoulEnum.KICKOFF, True),
-            foul_enum_name(FoulEnum.KICKOFF, False),
-            foul_enum_name(FoulEnum.FREE_BALL, True),
-            foul_enum_name(FoulEnum.FREE_BALL, False),
-            foul_enum_name(FoulEnum.GAME_ON),
-            foul_enum_name(FoulEnum.HALT)
+            get_foul_enum_name(FoulEnum.FREE_KICK, True),
+            get_foul_enum_name(FoulEnum.FREE_KICK, False),
+            get_foul_enum_name(FoulEnum.GOAL_KICK, True),
+            get_foul_enum_name(FoulEnum.GOAL_KICK, False),
+            get_foul_enum_name(FoulEnum.KICKOFF, True),
+            get_foul_enum_name(FoulEnum.KICKOFF, False),
+            get_foul_enum_name(FoulEnum.FREE_BALL, True),
+            get_foul_enum_name(FoulEnum.FREE_BALL, False),
+            get_foul_enum_name(FoulEnum.GAME_ON),
+            get_foul_enum_name(FoulEnum.HALT)
         ]
-
-        def get_state(
-            is_yellow: bool,
-            foul_enum: FoulEnum
-        ):
-            return get_state_name(is_yellow, foul_enum, self.is_yellow_team)
 
         transitions = [
             {
                 'trigger': get_trigger_name(FoulEnum.FREE_KICK, True),
                 'source': '*',
-                'dest': get_state(True, FoulEnum.FREE_KICK)
+                'dest': get_state_name(FoulEnum.FREE_KICK, True)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.FREE_KICK, False),
                 'source': '*',
-                'dest': get_state(False, FoulEnum.FREE_KICK)
+                'dest': get_state_name(FoulEnum.FREE_KICK, False)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.GOAL_KICK, True),
                 'source': '*',
-                'dest': get_state(True, FoulEnum.GOAL_KICK)
+                'dest': get_state_name(FoulEnum.GOAL_KICK, True)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.GOAL_KICK, False),
                 'source': '*',
-                'dest': get_state(False, FoulEnum.GOAL_KICK)
+                'dest': get_state_name(FoulEnum.GOAL_KICK, False)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.KICKOFF, True),
                 'source': '*',
-                'dest': get_state(True, FoulEnum.KICKOFF)
+                'dest': get_state_name(FoulEnum.KICKOFF, True)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.KICKOFF, False),
                 'source': '*',
-                'dest': get_state(False, FoulEnum.KICKOFF)
+                'dest': get_state_name(FoulEnum.KICKOFF, False)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.FREE_BALL, True),
                 'source': '*',
-                'dest': get_state(True, FoulEnum.FREE_BALL)
+                'dest': get_state_name(FoulEnum.FREE_BALL, True)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.FREE_BALL, False),
                 'source': '*',
-                'dest': get_state(False, FoulEnum.FREE_BALL)
+                'dest': get_state_name(FoulEnum.FREE_BALL, False)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.GAME_ON),
                 'source': '*',
-                'dest': foul_enum_name(FoulEnum.GAME_ON)
+                'dest': get_foul_enum_name(FoulEnum.GAME_ON)
             },
             {
                 'trigger': get_trigger_name(FoulEnum.HALT),
                 'source': '*',
-                'dest': foul_enum_name(FoulEnum.HALT)
+                'dest': get_foul_enum_name(FoulEnum.HALT)
             },
         ]
 
-        super(GameStateMachine, self).__init__(
-            model=model,
+        self.machine = Machine(
+            model=self.model,
             states=states,
             transitions=transitions,
-            initial=foul_enum_name(self.initial_state)
+            initial=get_foul_enum_name(self.initial_state)
         )
+
+    def set_state_by_referee_message(self, message: RefereeMessage):  
+        self.model.set_state_by_referee_message(message)
+
+    def get_state(self):
+        return self.model.state
--- lib/utils/game_state_machine_utils.py
+++ lib/utils/game_state_machine_utils.py
@@ -2,7 +2,7 @@ from lib.domain.enums.foul_enum import FoulEnum
 
 class GameStateMachineUtils:
     @staticmethod
-    def foul_enum_name(
+    def get_foul_enum_name(
         foul_enum: FoulEnum,
         is_team: 'bool | None' = None
     ):
@@ -14,11 +14,11 @@ class GameStateMachineUtils:
 
     @staticmethod
     def get_state_name(
-        is_yellow: bool,
         foul_enum: FoulEnum,
+        is_yellow: bool,
         is_yellow_team: bool
     ):
-        foul_enum_name = GameStateMachineUtils.foul_enum_name
+        foul_enum_name = GameStateMachineUtils.get_foul_enum_name
 
         if is_yellow == is_yellow_team:
             return f'{foul_enum_name(foul_enum)}_team'
@@ -29,10 +29,27 @@ class GameStateMachineUtils:
         foul_enum: FoulEnum,
         is_trigger_yellow: bool | None = None
     ):
-        foul_enum_name = GameStateMachineUtils.foul_enum_name
+        foul_enum_name = GameStateMachineUtils.get_foul_enum_name
 
         if is_trigger_yellow is None:
             return foul_enum_name(foul_enum)
         if is_trigger_yellow:
             return f'{foul_enum_name(foul_enum)}_yellow'
-        return f'{foul_enum_name(foul_enum)}_yellow'
+        return f'{foul_enum_name(foul_enum)}_blue'
+    
+    @staticmethod
+    def get_trigger_method_name(
+        foul_enum: FoulEnum,
+        is_yellow_team: bool
+    ):
+        team_foe_team_foul_enums = [
+            FoulEnum.FREE_KICK,
+            FoulEnum.GOAL_KICK,
+            FoulEnum.KICKOFF,
+            FoulEnum.FREE_BALL]
+        
+        foul_enum_name = GameStateMachineUtils.get_trigger_name
+
+        if foul_enum in team_foe_team_foul_enums:
+            return foul_enum_name(foul_enum, is_yellow_team)
+        return foul_enum_name(foul_enum)
